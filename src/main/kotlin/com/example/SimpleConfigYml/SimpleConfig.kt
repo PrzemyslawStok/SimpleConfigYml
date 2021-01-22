db4912c8756955db29753a570a616a9bd5f5f7f5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-22 19:21:45 +0100

    cd..

---
+++ src/main/kotlin/com/example/SimpleConfigYml/SimpleConfig.kt
@@ -0,0 +1,12 @@
+package com.example.SimpleConfigYml
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("app")
+class SimpleConfig {
+    var app: String? = null
+}
