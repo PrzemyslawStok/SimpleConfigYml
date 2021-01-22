cc527efd43e58680b97da359ce484dee7f0814b4
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-22 18:54:41 +0100

    init

---
+++ src/main/kotlin/com/example/SimpleConfigYml/SimpleConfigYmlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SimpleConfigYml
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleConfigYmlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleConfigYmlApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SimpleConfigYml/SimpleConfigYmlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SimpleConfigYml
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleConfigYmlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
